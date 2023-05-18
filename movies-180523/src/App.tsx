1704cb7678ab25c789090967d499ab62fd48e238
aaviki <avikianastasova@gmail.com>
2023-05-18 17:39:36 +0300

    counter

--- movies-180523/src/App.tsx
+++ movies-180523/src/App.tsx
@@ -2,11 +2,15 @@ import React from "react";
 // import logo from "./logo.svg";
 import "./App.css";
 import { Movies } from "./components/movies/Movies";
+import { Timer } from "./components/timer/Timer";
+import { Counter } from "./components/counter/Counter";
 
 function App() {
   return (
     <div className="App">
-      <Movies />
+      {/* <Movies /> */}
+      {/* <Timer /> */}
+      <Counter />
     </div>
   );
 }
