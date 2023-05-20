f3cb9f8cb2c066a1831fbe9e7d71fabbb348a8ab
aaviki <avikianastasova@gmail.com>
2023-05-20 11:20:57 +0300

    Services

--- movies-180523/src/App.tsx
+++ movies-180523/src/App.tsx
@@ -10,9 +10,9 @@ function App() {
   return (
     <div className="App">
       {/* <Movies /> */}
-      <Timer />
+      {/* <Timer /> */}
       {/* <Counter /> */}
-      {/* <Todos /> */}
+      <Todos />
     </div>
   );
 }
--- movies-180523/src/todos/Todos.tsx
+++ movies-180523/src/todos/Todos.tsx
@@ -1,24 +1,32 @@
 import { useEffect, useState } from "react";
 import { Todo } from "../todo/Todo";
+import { todosService } from "../services/todo";
 
 export function Todos() {
   const [todos, setTodos] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
+  // useEffect(() => {
+  //   fetch("https://jsonplaceholder.typicode.com/todos")
+  //     .then((response) => response.json())
+  //     .then((result) => setTodos(result))
+  //     .catch((error) => console.log(error))
+  //     .finally(() => {
+  //       console.log("Done");
+  //       setIsLoading(false);
+  //     });
+  // }, []);
+
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/todos")
-      .then((response) => response.json())
+    todosService
+      .getAllTodos()
       .then((result) => setTodos(result))
-      .catch((error) => console.log(error))
-      .finally(() => {
-        console.log("Done");
-        setIsLoading(false);
-      });
+      .finally(() => setIsLoading(false));
   }, []);
 
   return (
     <div>
-      <h1 style={{ backgroundColor: "red" }}>Todos here</h1>
+      <h1 /* style={{ backgroundColor: "red" }} */>Todos here</h1>
       {isLoading && <p>Loading...</p>}
       {/* {todos.length > 0 && todos.map((el) =>  <div>{el["title"]}</div>)} */}
       {/* {todos.length > 0 && todos.map((el) => <Todo todo={el} key={el["id"]} />)} */}
