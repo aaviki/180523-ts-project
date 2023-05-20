4b79ba78276314fba8b0e5175fe2218143bb4f40
aaviki <avikianastasova@gmail.com>
2023-05-20 21:39:48 +0300

    230520 end of the day

--- movies-180523/src/components/home/home.tsx
+++ movies-180523/src/components/home/home.tsx
@@ -1,9 +1,14 @@
+import { useLocation } from "react-router-dom";
 import { Search } from "../search/search";
 
 export function Home() {
+  const location = useLocation();
+
   return (
     <>
       <h1>Home here</h1>
+      {location.state && <p>User is logged in</p>}
+      {!location.state && <p>User is not logged in</p>}
       <Search />
     </>
   );
--- movies-180523/src/components/login/Login.tsx
+++ movies-180523/src/components/login/Login.tsx
@@ -1,18 +1,77 @@
-import { useLocation, Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useLocation, Link, useNavigate } from "react-router-dom";
 
 export function Login() {
   const location = useLocation();
   console.log(location);
+  
+  const [usernameError, setUsernameError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [hasUser, setHasUser] = useState(false);
+  const navigate = useNavigate;
+  
+  const onLogin = (e: any) => {
+    e.preventDefault();
+    console.log("event: ", e.target);
+    const formData = new FormData(e.target);
+    
+    const username = String(formData.get("username"));
+    const password = String(formData.get("password"));
+    const repass = String(formData.get("repass"));
+    console.log(username, password, repass);
+    
+    useEffect(() => {
+      if (hasUser) {
+        navigate("/", {
+          state: hasUser
+        });
+      };
+    },[hasUser])
+    
+    if (username.length < 3) {
+      return setUsernameError("<3");
+    } else {
+      setUsernameError("");
+    }
+
+    if (password.length < 5) {
+      return setPasswordError(">=5");
+    } else if (password.includes("&")) {
+      return setPasswordError("can't");
+    } else {
+      setPasswordError("");
+    }
+
+    setHasUser(true);
+    e.target.reset()
+
+      
+
   return (
     <>
       <h1>Login Here</h1>
       {location.state && <p>this To do is comleted, you can Sign in</p>}
-      {!location.state && (
-        <p>
-          this To do is not finished yet comleted, please{" "}
-          <Link to="/register">Register</Link>
-        </p>
-      )}
+      {/* {!location.state && ( */}
+
+      {hasUser && <h1> User is loged in</h1>}
+      <form onSubmit={onLogin}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input type="text" id="username" name="username" />
+        </div>
+        <div>{usernameError.length > 0 && <p>Username is required</p>}</div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" />
+        </div>
+        <div>{passwordError.length > 0 && <p>{passwordError}</p>}</div>
+        <div>
+          <label htmlFor="repass">Repeat password:</label>
+          <input type="password" id="repass" name="repass" />
+        </div>
+        <input type="submit" value="Login" />
+      </form>
+      {/* )} */}
     </>
   );
 }
